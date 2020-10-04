f8637b02cffb49376f85212e09ea32c6515cbd2f
gfonseca18 <gercia.fonseca@icloud.com>
2020-10-04 22:55:28 +0100

    Adicionar arquivos de projeto.

---
+++ SistemaAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
